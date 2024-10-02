071e7481e2608ce1d88011769c7d6c37f9a77543
John Doe <you@example.com>
2024-10-02 16:42:17 +0300

    Test passed

--- MyMatrix.js
+++ MyMatrix.js
@@ -4,5 +4,9 @@ exports.equal = function equal(m1, m2){
 }
 
 exports.add = function add(m1, m2){
-    return [[2,0,0],[0,0,0],[0,0,0]]
+    return m1.map(function (row, row_i) {
+        return row.map(function (num, num_i){
+            return num + m2[row_i][num_i];
+        });
+      });
 }
