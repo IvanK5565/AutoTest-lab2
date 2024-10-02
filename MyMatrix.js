3d433735e1598032c5bd8ef024bd414b8a57b97f
John Doe <you@example.com>
2024-10-02 16:34:10 +0300

    Test for add

--- MyMatrix.js
+++ MyMatrix.js
@@ -2,3 +2,7 @@
 exports.equal = function equal(m1, m2){
     return JSON.stringify(m1) == JSON.stringify(m2);
 }
+
+exports.add = function add(m1, m2){
+    
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-const { equal }  = require('../MyMatrix.js');
+const { equal, add }  = require('../MyMatrix.js');
 
 describe('MyMatrix', function () {
   describe("compare", function () {
@@ -23,6 +23,26 @@ describe('MyMatrix', function () {
     ]
     it('must not be equal', function(){
       assert.ok(false == equal(m1,m3))
+    })
+  })
+  describe("adding", function(){
+    it("must be \n2,0,0\n0,0,0\n0,0,0", function(){
+      m1 = [
+        [1,0,0],
+        [0,0,0],
+        [0,0,0]
+      ]
+      m2 = [
+        [1,0,0],
+        [0,0,0],
+        [0,0,0]
+      ]
+      m3 = [
+        [2,0,0],
+        [0,0,0],
+        [0,0,0]
+      ]
+      assert.ok(equal(add(m1,m2),m3))
     })
   })
 });
